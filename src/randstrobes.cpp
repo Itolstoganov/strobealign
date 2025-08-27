431638fd1ffa7338a98e46dd8b49f8821638cf84
Itolstoganov <itolstoganov@gmail.com>
2025-08-27 13:23:05 +0200

    Remove last 8 bits from the randstrobe hash for fair comparison

--- src/randstrobes.cpp
+++ src/randstrobes.cpp
@@ -46,7 +46,7 @@ static inline syncmer_hash_t syncmer_smer_hash(uint64_t packed) {
 }
 
 static inline randstrobe_hash_t randstrobe_hash(syncmer_hash_t hash1, syncmer_hash_t hash2) {
-    return hash1 + hash2;
+    return hash1 + hash2 << 8;
 }
 
 std::ostream& operator<<(std::ostream& os, const Syncmer& syncmer) {
